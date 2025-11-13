92d0edad3616f2f130102acd72e9da67a365e436
Cappucciyes <ltkproper@gmail.com>
2025-11-14 00:07:47 +0900

    implemeneted door

--- Assets/Map/Door.cs
+++ Assets/Map/Door.cs
@@ -5,12 +5,18 @@ public class Door : NetworkBehaviour, IInteractable
 {
     public NetworkVariable<bool> isOpen = new NetworkVariable<bool>(false);
     public int doorId { get; private set; }
+    private SpriteRenderer spriteRenderer;
+    private BoxCollider2D boxCollider2D; 
+    [SerializeField] private Sprite closedSprite;
+    [SerializeField] private Sprite opendSprite;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public override void OnNetworkSpawn()
     {
         base.OnNetworkSpawn();
         doorId = GlobalHelper.GenerateUniqueID();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        boxCollider2D = GetComponent<BoxCollider2D>();
     }
 
 
@@ -18,6 +24,7 @@ public class Door : NetworkBehaviour, IInteractable
     public void Interact()
     {
         if (!CanInteract()) return;
+        print("is interacting");
         isOpen.Value = !isOpen.Value;
         if (isOpen.Value)
             openDoor();
@@ -32,10 +39,15 @@ public class Door : NetworkBehaviour, IInteractable
 
     private void openDoor()
     {
-
+        print("isOpened");
+        boxCollider2D.enabled = false;        
+        spriteRenderer.sprite = opendSprite;
     }
     private void closeDoor()
     {
-
+        print("isClosed");
+        boxCollider2D.enabled = false;        
+        boxCollider2D.enabled = true;        
+        spriteRenderer.sprite = closedSprite;
     }
 }
--- Assets/PlayerScript/InteractionDetector.cs
+++ Assets/PlayerScript/InteractionDetector.cs
@@ -1,16 +1,47 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class InteractionDetector : MonoBehaviour
 {
+    private IInteractable currentInteractableObject = null;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void OnInteract(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            print("did stuff");
+            currentInteractableObject?.Interact();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("collided with " + collision.gameObject.name);
+        if (collision.TryGetComponent(out IInteractable interactable) && interactable.CanInteract())
+        {
+            currentInteractableObject = interactable;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        print("Now leaving behind" + collision.gameObject.name);
+        if (collision.TryGetComponent(out IInteractable interactable) && interactable == currentInteractableObject)
+        {
+            currentInteractableObject = null;
+        }
     }
 }
